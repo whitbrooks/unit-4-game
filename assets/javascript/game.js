0dfcc0fc1b6bc71f0618ae891e2434cf5b160df9
whitbrooks <whitbrooks@gmail.com>
2018-06-29 23:56:21 -0700

    buttons produce and display numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,82 @@
 // VARIABLES
 var dealerArray = [];
-var dealerNumber = "";
+var dealerNumber = 0;
+var animalArray = [];
+var yourScore = 0;
+var donkey = 0;
+var cow = 0;
+var goat = 0;
+var hen = 0;
 
 // // load html before running script
 $(document).ready(function() {
 
-  // Generate, store and display random computer selection between 19 - 120
+  // Generate array of options for "dealer" between 19 - 120
   for (var i = 19; i <= 120; i++) {
     dealerArray.push(i);
   }
 
+  // Generate array of options for animals between 1 - 12
+  for (var i = 1; i <= 12; i++) {
+    animalArray.push(i);
+  }
+
   // user clicks to start game
   $("#start").click(function() {
-    
-  var dealerNumber = dealerArray[Math.floor(Math.random()*dealerArray.length)];
-    $("#randomNumber").text(dealerNumber);
+  
+      // select and display random number for dealer
+      var dealerNumber = dealerArray[Math.floor(Math.random()*dealerArray.length)];
+        $("#randomNumber").html(dealerNumber);
 
+      // select and store random numbers for each animal
+      // var donkey = animalArray[Math.floor(Math.random()*animalArray.length)];
+      // console.log(donkey);
+
+      // var cow = animalArray[Math.floor(Math.random()*animalArray.length)];
+      // console.log(cow);  
+
+      // var goat = animalArray[Math.floor(Math.random()*animalArray.length)];
+      // console.log(goat);
+
+      // var hen = animalArray[Math.floor(Math.random()*animalArray.length)];
+      // console.log(hen);
+    
   });
 
-});
-// Generate, store and hide random number for each animal between 1 - 12
+  // Create on-click events for each animal button that increase "your score"
+  $("#Donkey").click(function() {
+      var donkey = animalArray[Math.floor(Math.random()*animalArray.length)];
+      console.log(donkey);
+      donkey = parseInt(donkey);
+      yourScore += donkey;
+      $("#myNumber").html(yourScore);
+  });
+  
+  $("#Cow").click(function() {
+    var cow = animalArray[Math.floor(Math.random()*animalArray.length)];
+    console.log(cow);
+    cow = parseInt(cow);
+    yourScore += cow;
+    $("#myNumber").html(yourScore);
+  });
 
-// Create on-click events for each animal button that increase "your score"
+  $("#Goat").click(function() {
+    var goat = animalArray[Math.floor(Math.random()*animalArray.length)];
+    console.log(goat);
+    goat = parseInt(goat);
+    yourScore += goat;
+    $("#myNumber").html(yourScore);
+  });
+
+  $("#Hen").click(function() {
+    var hen = animalArray[Math.floor(Math.random()*animalArray.length)];
+    console.log(hen);
+    hen = parseInt(hen);
+    yourScore += hen;
+    $("#myNumber").html(yourScore);
+  });
+
+  
 
 // Check if "your score" equals or exceeds computer selection
 
@@ -30,3 +85,4 @@ $(document).ready(function() {
 // Reset game without reseting wins and loses
 
 // Cap wins and loses at 10
+});
